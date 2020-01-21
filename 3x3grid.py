57705afb3f91b4d8d609cb9d8e6fc49e88f45ca2
GutiGod <57073572+GutiGod@users.noreply.github.com>
2020-01-21 03:32:05 -0300

    Add files via upload
    Establishing valid numbers for a single 3x3 sub-grid

---
+++ 3x3grid.py
@@ -0,0 +1,54 @@
+from turtle import Screen, Turtle   
+import random
+
+#number of squares and size in pixels
+N = 9
+Size = 30
+
+def grid (turtle, N, Size):
+    sign = 1
+    for _ in range(2):
+        for _ in range(N):
+            turtle.forward(Size * N)
+            turtle.left(sign * 90)
+            turtle.forward(Size)
+            turtle.left(sign * 90)
+            sign = 0 - sign
+
+        turtle.forward(Size * N)
+        [turtle.right, turtle.left][N % 2](90)
+        sign = 0 - sign
+
+screen = Screen()
+yertle = Turtle()
+
+yertle.penup()
+yertle.goto(-N * Size/2, -N * Size/2)  # center our grid (optional)
+yertle.pendown()
+
+grid(yertle, N, Size)
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+last = 8
+lon = 11
+lar = 4
+lap = 3
+for _ in range(9):
+    numind = random.randint(0,last)
+    print (numind)
+    h = numbers[numind] 
+    numbers.remove(h)
+    last = last - 1
+    yertle.penup()
+    yertle.goto(-N * Size/2 + lon, -N * Size/2 + lar)
+    yertle.pendown()
+    yertle.write(h, move = False, font= ("Arial", 15, "normal"))
+    lap = lap - 1   
+    if lap == 0:
+        lon = 11
+        lar = lar + Size
+        lap = 3
+    else:
+        lon = lon + Size    
+
+screen.exitonclick()
