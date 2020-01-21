2ee60db731a1a60dceef3eba61353aaec9444046
GutiGod <57073572+GutiGod@users.noreply.github.com>
2020-01-21 20:48:30 -0300

    Update Sudokualgorithm.py
    Correctly creates numbers for 9 subgrids of 3x3. Still not dependant on each other.

--- Sudokualgorithm.py
+++ Sudokualgorithm.py
@@ -4,7 +4,7 @@ laps = 0
 subgridnum = 0
 subgrid = {0: 0 , 1: 0 , 2: 0 , 3: 0 , 4: 0 , 5: 0 , 6: 0 , 7: 0 , 8: 0}
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-subgriddic = {0: 0, 1:0 , 2:0 , 3:0 , 4:0 , 5:0 , 6:0 , 7:0 , 8:0 , 9:0 }
+subgriddic = {0: 0, 1:0 , 2:0 , 3:0 , 4:0 , 5:0 , 6:0 , 7:0 , 8:0 }
 
 def resetnums(numbers, latnums1, latnums2, latnums3, laps):
     numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -14,16 +14,19 @@ def resetnums(numbers, latnums1, latnums2, latnums3, laps):
     laps = 0
 
 def numpull(numbers, laps, subgridnum):
-    for _ in range (9):
-        num = random.choice(numbers)
-        print(num)
-        numbers.remove(num)
-        subgrid[laps] = num
-        laps = laps + 1    
-    subgriddic[subgridnum] = subgrid  
-    subgridnum = subgridnum + 1
+    for _ in range(9):
+        laps = 0
+        numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        subgrid = {0: 0 , 1: 0 , 2: 0 , 3: 0 , 4: 0 , 5: 0 , 6: 0 , 7: 0 , 8: 0}
+        for _ in range (9):
+            num = random.choice(numbers)
+            numbers.remove(num)
+            subgrid[laps] = num
+            laps = laps + 1    
+        subgriddic[subgridnum] = subgrid  
+        subgridnum = subgridnum + 1
+        print (subgrid)
+
     print (subgriddic)
 
-resetnums(0, 0, 0, 0, laps)
-
-numpull(numbers, laps, subgridnum)
+numpull(0,0,0)
